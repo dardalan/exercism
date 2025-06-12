0f8c0990ddb71abb34dedeea6b34092016e69ff8
dardalan <svyat.9191@gmail.com>
2025-06-12 23:45:39 +0200

    process registration task

---
+++ elixir/process-register/README.md
@@ -0,0 +1,15 @@
+# ProcessRegister
+
+Exercise Register process to demonstrate process registration
+
+## Running tests
+
+Execute the tests with:
+
+```bash
+$ elixir process_register_test.exs
+```
+
+## Source
+
+Simple Register process exercise to demonstrate how to register process
---
+++ elixir/process-register/process_register.exs
@@ -0,0 +1,35 @@
+defmodule ProcessRegister do
+  use Agent
+
+  def start_link(initial_value \\ %{}) do
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  def add(process, name) when is_pid(process) do
+    if registrable?(process, name) do
+      Process.register(process, name)
+      Agent.update(__MODULE__, fn state -> Map.put(state, name, process) end)
+    end
+
+    :ok
+  end
+
+  def drop(name) do
+    try do
+      Process.unregister(name)
+      :ok
+    rescue
+     _ -> :ok
+    after
+      Agent.update(__MODULE__, fn state -> Map.delete(state, name) end)
+    end
+  end
+
+  def list_registered() do
+    Agent.get(__MODULE__, fn state -> state end)
+  end
+
+  defp registrable?(pid, name) do
+    !Map.has_key?(list_registered(), name) && Process.alive?(pid)
+  end
+end
